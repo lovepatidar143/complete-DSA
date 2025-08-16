aab08c905101d3c014b7dfa281e0aa1acbb390b8
love patidar <engrlovepatidar@gmail.com>
2025-08-16 20:12:12 +0530

    done

---
+++ step 3/midium array/markZero.cpp
@@ -0,0 +1,43 @@
+#include <iostream> 
+using namespace std;
+void zeros(vector <vector<int>> &matrix){
+    //matrix[..][0] for the rowtracking
+    //matrix[0][..] for col tracking
+
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int col0 =1;
+
+    for(int i= 0 ; i<n ; i++){
+        for(int j =0; j<m ; j++){
+            if(matrix[i][j]==0){
+                //mark row 
+                matrix[i][0] = 0;
+
+                // mark col
+                if(j== 0) col0 = 0;
+                else matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for(int i =1 ; i<n ; i++){
+        for(int j =1 ; j<m ; j++){
+            if(matrix[0][j]==0 || matrix[i][0]==0) matrix[i][j]= 0;
+        }
+    }
+    // for col traker
+    if(matrix[0][0]==0){
+        for(int j = 1 ; j<m ; j++){
+            matrix[0][j] =0;
+        }
+    }
+    if(col0==0){
+        for(int i =0; i< n ; i++){
+            matrix[i][0]=0;
+        }
+    }
+}
+int main(){
+    return 0;
+}
